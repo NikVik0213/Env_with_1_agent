950961b264c2fbb1f6f796c27954f41cd4fb9d15
NikVik0213 <nik.vik.gorbenko@gmail.com>
2024-11-26 22:55:35 +0300

    Delete logger.py

--- logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logging.basicConfig(
-    filename='logging.log',
-    level=logging.DEBUG,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
