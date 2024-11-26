ae55b7d2ad170bc210089247a91eeaecaf0aced9
NikVik0213 <nik.vik.gorbenko@gmail.com>
2024-11-26 22:54:58 +0300

    Delete check_env.py

--- check_env.py
+++
@@ -1,7 +0,0 @@
-from stable_baselines3.common.env_checker import check_env
-from Watering_start_Env import WateringEnv
-
-
-env = WateringEnv()
-# It will check your custom environment and output additional warnings if needed
-check_env(env)
